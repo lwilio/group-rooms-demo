990ed78e1768f5337b779f06e66b97bd351eadf0
lwilio <llopez@twilio.com>
2017-05-12 17:35:56 +0200

    Improved look&feel of remote video rendering

--- public/client.js
+++ public/client.js
@@ -4,15 +4,15 @@ var Video = Twilio.Video;
 var room;
 var enableVideo = true;
 var enableAudio = true;
+var numVideoTracks = 0;
 
 window.addEventListener("load", function() {
 
     var socket = io();
-
     socket.on('accessToken', connectToRoom);
-
     socket.emit('getAccessToken');
 
+    //Support for only viewers: participats that don't publish new tracks but subscribe to existing tracks
     if (window.location.href.search("role=onlyViewer") > 0) {
         enableVideo = false;
         enableAudio = false;
@@ -50,32 +50,40 @@ function connectToRoom(msg) {
 }
 
 function onParticipantConnected(participant) {
-
-    participant.on('trackAdded', function(track) {
-        var mediaElement = track.attach();
-        document.getElementById('divRemoteVideoContainer').appendChild(mediaElement);
-    });
-
-    participant.on('trackRemoved', function(track) {
-        track.detach().forEach(function(el) {
-            el.remove();
-        });
-    });
-
+    participant.on('trackAdded', attachTrack);
+    participant.on('trackRemoved', detachTrack);
     updateNumParticipants();
 }
 
 function onParticipantDisconnected(participant) {
-    participant.tracks.forEach(function(track) {
-        track.detach().forEach(function(el) {
-            el.remove();
-        });
-    });
-
+    participant.tracks.forEach(detachTrack);
     updateNumParticipants();
+}
+
+function attachTrack(track){
+  var mediaElement = track.attach();
+  document.getElementById('divRemoteVideoContainer').appendChild(mediaElement);
+  if(track.kind == 'video') updateDisplay(1);
+}
+
+function detachTrack(track){
+  track.detach().forEach(function(el) {
+      el.remove();
+  });
+  if(track.kind == 'video') updateDisplay(-1)
 }
 
 function updateNumParticipants() {
     var labelNumParticipants = document.getElementById('labelNumParticipants');
     labelNumParticipants.innerHTML = room.participants.size + 1;
+}
+
+function updateDisplay(num){
+  numVideoTracks += num;
+  var videoTagWidth = 100/(1+numVideoTracks);
+
+  var remoteVideoTags = document.querySelectorAll("#divRemoteVideoContainer video")
+  remoteVideoTags.forEach(function(videoTag){
+    videoTag.style.width = + videoTagWidth + '%';
+  });
 }
